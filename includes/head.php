a459eb8935acb369d540e8e4b11c64e5b15c68f9
dobish <33222646+dobish@users.noreply.github.com>
2018-02-19 21:12:36 +0100

    Add files via upload

---
+++ includes/head.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en-UK">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Macaronithon</title>
+    <!-- Description -->
+
+	<!-- Keywords -->
+	<meta name="keywords" content="HTML,CSS,JavaScript">
+
+	<!-- Favicons -->
+	<link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+
+
+    <!-- CSS & SASS -->
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <!-- CSS & SASS & Fonts-->
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link href="https://fonts.googleapis.com/css?family=Freckle+Face" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/awesomplete.css"></link>
+	<link rel="stylesheet" type="text/css" href="css/window-alert.css"></link>
+</head>
+
+<body>
